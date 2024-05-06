bcd1f1cb6182b953294dd454b26c172cad3720f8
EstebanUmba <eumbarila@unal.edu.co>
2024-05-06 17:19:08 -0500

    SOAP proyect routes set and app dockerized

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker build -t perfectpick_int_rp .
 
-docker run --network perfectpicknetwork -p 77:77 perfectpick_int_rp
+docker run --network perfectpicknetwork --name perfectpick_int_rp -t -p 77:77 perfectpick_int_rp
